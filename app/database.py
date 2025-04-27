737f1424a1210a31f1390d9d3020acf184094177
Gustavo Ranci <rancigustavo@gmail.com>
2025-04-27 10:23:08 -0300

    ajuste

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root@localhost/mestradoER"
+# DATABASE_URL = "mysql+pymysql://root@localhost/mestrado"
+DATABASE_URL = "mysql+pymysql://mestradoer@Ecidir88#/mestradoER"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
