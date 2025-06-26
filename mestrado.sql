ed934bee4674525e304633fe618c3aab6ac83640
Gustavo Ranci <rancigustavo@gmail.com>
2025-06-26 18:16:39 -0300

    Atualização de banco

--- mestrado.sql
+++ mestrado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/05/2025 às 21:26
+-- Tempo de geração: 26/06/2025 às 23:15
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
